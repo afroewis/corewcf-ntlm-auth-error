21b9a017f26d2c55ea0f9b0d5918d8ef148c07bc
AndreasFröwis <andreas.froewis@m-f.ch>
2025-10-27 11:41:42 +0100

    Initial, working version

---
+++ MyClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ServiceModel.Security;
+using MyClient.Service;
+
+Console.WriteLine("Hello, World!");
+
+var x = new ServiceClient();
+x.ClientCredentials.ServiceCertificate.SslCertificateAuthentication =
+    new X509ServiceCertificateAuthentication()
+    {
+        CertificateValidationMode = X509CertificateValidationMode.None,
+        RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck
+    };
+Console.WriteLine("Calling WCF Method GetData");
+var ret = await x.GetDataAsync(5);
+Console.WriteLine("Return of GetData: {0}", ret);
